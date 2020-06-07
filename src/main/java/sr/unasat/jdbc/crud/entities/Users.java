a85963175cbe4c356e214197e351187908522931
Heelmatt21 <Magenta340>
2020-06-06 21:55:03 -0300

    Users

--- src/main/java/sr/unasat/jdbc/crud/entities/Users.java
+++ src/main/java/sr/unasat/jdbc/crud/entities/Users.java
@@ -5,37 +5,45 @@ public class Users {
     private int user_id;
     private String username;
     private String password;
-    private int roll_id;
+    private int role_id;
 
     public Users(int user_id , String username , String password , int roll_id){
         this.user_id = user_id;
         this.username = username;
         this.password = password;
-        this.roll_id = roll_id;
+        this.role_id = role_id;
 
     }
 
-    public Users (int user_id) {
+    public void setuser_Id(int user_id) {
         this.user_id = user_id;
     }
 
-    public Users (String username) {this.username = username; }
-    public Users (String password) {this.password = password; }
-    public Users (int roll_id) {this.roll_id = roll_id; }
+    public void setusername(String username) {
+        this.username = username;
+    }
+
+    public void setpassword (String password) {
+        this.password = password;
+    }
+
+    public void setrole_id(int roll_id) {
+        this.role_id = role_id;
+    }
 
     public int getUser_id() { return user_id; }
     public String getUsername() { return username; }
     public String getPassword() { return password; }
-    public int getRoll_id() { return roll_id; }
+    public int getRole_id() { return role_id; }
 
     public void setUser_id(int user_id) { this.user_id = user_id; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
-    public void setRoll_id(int roll_id) { this.roll_id = roll_id; }
+    public void setRole_id(int role_id) { this.role_id = role_id; }
 
-    @Override
+    /*@Override
     public String toString() {
         return "Users{" + "user_id =" + user_id + ", username =" + username + ", password = " + password +
-                ", roll_id = " + roll_id };
-    }
+                ", role_id = " + role_id };
+  */
 }
--- src/main/java/sr/unasat/jdbc/crud/repositories/UsersRepository.java
+++ src/main/java/sr/unasat/jdbc/crud/repositories/UsersRepository.java
@@ -1,4 +1,115 @@
 package sr.unasat.jdbc.crud.repositories;
 
+import sr.unasat.jdbc.crud.entities.Memberships;
+import sr.unasat.jdbc.crud.entities.Persoon;
+import sr.unasat.jdbc.crud.entities.Users;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsersRepository {
+    private Connection connection;
+    public UsersRepository(){
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("De driver is geregistreerd!");
+
+        String URL = "jdbc:mysql://localhost/adres_boek";
+        String USER = "root";
+        String PASS = "";
+        connection = DriverManager.getConnection(URL, USER, PASS);
+        System.out.println(connection);
+    } catch (ClassNotFoundException ex) {
+        System.out.println("Error: unable to load driver class!");
+        System.exit(1);
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    }
+
+    public List<Users> findAllRecords() {
+        List<Users> usersList = new ArrayList<Users>();
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            String sql = "select * from users";
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("resultset: " + rs);
+            while (rs.next()) {
+                int user_id = rs.getInt("user_id");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                int role_id = rs.getInt("role_id");
+                usersList.add(new Users(user_id , username , password , role_id));
+            }
+            rs.close();
+
+
+        } catch (SQLException e) {
+
+        } finally {
+
+        }
+        return usersList;
+    }
+
+    public int insertOneRecord(Users users) {
+        PreparedStatement stmt = null;
+        int result = 0;
+        try {
+            String sql = "insert into users (username , password) values(? , ?)";
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, users.getUsername());
+            stmt.setString(2, users.getPassword());
+            result = stmt.executeUpdate();
+            System.out.println("resultset: " + result);
+
+        } catch (SQLException e) {
+
+        } finally {
+
+        }
+        return result;
+    }
+
+    public int deleteOneRecord(Users users){
+        PreparedStatement stmt = null;
+        int result = 0;
+        try {
+            String sql = "DELETE FROM users WHERE user_id = ?";
+            stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, users.getUser_id());
+            result = stmt.executeUpdate();
+            System.out.println("deleted: " + users.getUser_id());
+
+        } catch (SQLException e) {
+
+        } finally {
+
+        }
+        return result;
+    }
+    public int updateMembershipRecord(Users users) {
+        PreparedStatement stmt = null;
+        int result = 0;
+        try {
+            String sql = "update users set username = ? password = ? where user_id = ?";
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, users.getUsername());
+            stmt.setString(2, users.getPassword());
+            stmt.setInt(3, users.getUser_id());
+            result = stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("An error has occurred ");
+        } finally {
+
+        }
+        return result;
+    }
+
+
+
+
 }
