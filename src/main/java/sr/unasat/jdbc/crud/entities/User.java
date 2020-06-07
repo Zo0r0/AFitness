6f24f737f8bccb9fae5957ba9eef7fce7ded9365
Heelmatt21 <Magenta340>
2020-06-07 20:30:39 -0300

    Users

--- src/main/java/sr/unasat/jdbc/crud/entities/User.java
+++ src/main/java/sr/unasat/jdbc/crud/entities/User.java
@@ -5,25 +5,25 @@ public class User {
     public int user_id;
     public String username;
     public String password;
-    public User user;
+    public Role role;
 
     public User(int user_id , String username , String password , int role_id){
         this.user_id = user_id;
         this.username = username;
         this.password = password;
-        this.user = user;
+        this.role = role;
 
     }
 
     public int getUser_id() { return user_id; }
     public String getUsername() { return username; }
     public String getPassword() { return password; }
-    public User getUser() { return user; }
+    public Role getRole() { return role; }
 
     public void setUser_id(int user_id) { this.user_id = user_id; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
-    public void setUser(User user) { this.user = user; }
+    public void setRole(User user) { this.role = role; }
 
     /*@Override
     public String toString() {
