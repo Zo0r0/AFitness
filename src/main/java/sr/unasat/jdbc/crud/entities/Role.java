1b9da4e3ac9e90d88fac4359c693fa907f725e5a
Rainesh <s.sewdien@unasat.sr>
2020-06-06 21:09:06 -0300

    repo red

--- src/main/java/sr/unasat/jdbc/crud/entities/Role.java
+++ src/main/java/sr/unasat/jdbc/crud/entities/Role.java
@@ -1,27 +1,25 @@
 package sr.unasat.jdbc.crud.entities;
 
 public class Role {
-private int role_id;
-private static String role_name;
+    private static String role_name;
 private static String access_level;
 
-public Role (int role_id, String name, String access_level)
+public Role (int role_id, String role_name, String access_level)
 {
-    this.role_id =       role_id;
     role_name =          role_name;
     Role.access_level =   access_level;
 }
 
-    public Role(int role_id)
-{   this.role_id=role_id;
-}
+    public Role(int role_id){
+
+    }
 
     public static void getaccess_level() {
 
     }
     public static void access_level() {
     }
-    public static int getName() {
+    public static String getName() {
         return role_name;
     }
     public static String getAccess_level() {
@@ -30,19 +28,12 @@ public Role (int role_id, String name, String access_level)
     public void role_name (String role_name)
     { Role.role_name =role_name;
 }
-    public void access_level(String access_level)
-{     Role.access_level =access_level;
-}
 
     public void setRole_id(int role_id)
-{   this.role_id=role_id;
+{
 }
     public void setName(String name) {
-    role_name = role_name;
-}
-    public void setAcces_level(String access_level) {
-    Role.access_level = access_level;
-
+    role_name =  role_name;
 }
 
     /*
